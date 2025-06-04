8673901af41c21c184719311e1fd0ad07efac910
User_PC-Oren4 <User_PC-Oren4@PC-OREN4>
2025-06-04 21:23:27 +0500

    Добавьте файлы проекта.

---
+++ CalculationLib.dll/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+namespace CalculationLib.dll
+{
+    public class Class1
+    {
+        public static double Plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            {
+                return a / b;
+            }
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
---
+++ ConsoleApp34/Program.cs
@@ -0,0 +1,16 @@
+﻿using CalculationLib.dll;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        {
+            Console.WriteLine($"Plus: {Class1.Plus(12, 18)}");
+            Console.WriteLine($"Minus: {Class1.Minus(777, 100)}");
+            Console.WriteLine($"Multiply: {Class1.Multiply(56, 36)}");
+            Console.WriteLine($"Divide: {Class1.Divide(404, 228)}");
+
+
+        }
+    }
+}
